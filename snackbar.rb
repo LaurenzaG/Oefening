7e487ca69da183a80e2be4372db6d806eeefaded
LaurenzaG <l.gradenwitz@gmail.com>
2017-10-22 12:20:41 +0200

    ask user for name

--- snackbar.rb
+++ snackbar.rb
@@ -1,4 +1,11 @@
 def initialize_snackbar
-  @patat speciaal = 2.0
-  @kroket = 1.0
-  @
+  @patat_speciaal = 2.0
+  @patat_mayonaise = 1.0
+  @patat_pindasaus = 1.5
+  @patat_currysaus = 2.5
+end
+
+def customer_name
+  print "what's your name? "
+  return gets.chomp
+end
